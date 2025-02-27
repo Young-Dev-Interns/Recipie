fc37e4e868d825d7a87eb9f9e7ddbcbf9ded37ad
Young-Dev-Interns <147915034+Young-Dev-Interns@users.noreply.github.com>
2025-02-27 22:08:53 +0500

    Basic Files without Node Modules
    //

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './index.css'
+
+
+function App() {
+
+  return (
+    <>
+      <div className='text-red-500'>
+        Hi
+       </div>
+    </>
+  )
+}
+
+export default App
